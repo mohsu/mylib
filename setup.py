264f46f098569822520c6cee399e53d057c43295
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-06-30 17:41:02 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mylib',
+    version='1.0',
+    packages=['utils', 'utils.label', 'utils.label.tool'],
+    url='git@github.com:mohsu/mylib.git',
+    license='',
+    author='Maureen Hsu',
+    author_email='',
+    description=''
+)
