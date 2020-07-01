9b264aa5bfb7483b8f638d4607a6b2ef419ff2d0
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-07-01 10:36:51 +0800

    add requirements

--- setup.py
+++ setup.py
@@ -15,5 +15,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['python=3.7.7', 'tensorflow=2.1.0', 'pynvml', 'requests', 'loguru=0.4.1', 'imgaug=0.4.0',
+                      'numpy=1.18.1', 'matplotlib=3.2.1', 'opencv=4.2.0'],
     python_requires='>=3.7',
 )
