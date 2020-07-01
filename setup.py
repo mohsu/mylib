9b0943738f4033546a3f66b0a8504a494b9e5f19
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-07-01 10:44:02 +0800

    edit version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mylib',
-    version='1.0',
+    version='1.1',
     packages=setuptools.find_packages(),
     url='https://github.com/mohsu/mylib',
     license='',
@@ -15,7 +15,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['python=3.7.7', 'tensorflow=2.1.0', 'pynvml', 'requests', 'loguru=0.4.1', 'imgaug=0.4.0',
-                      'numpy=1.18.1', 'matplotlib=3.2.1', 'opencv=4.2.0'],
+    install_requires=['tensorflow>=2.1.0', 'pynvml', 'requests', 'loguru>=0.4.1', 'imgaug>=0.4.0',
+                      'numpy>=1.18.1', 'matplotlib>=3.2.1', 'python-opencv>=4.2.0'],
     python_requires='>=3.7',
 )
