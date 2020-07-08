7c52f6a6177b611d045d9236c70e5c2686300e37
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-07-08 16:00:31 +0800

    change int(np.floor) to //

--- utils/label/data_gen.py
+++ utils/label/data_gen.py
@@ -37,7 +37,7 @@ class VOCDataGenerator(keras.utils.Sequence):
 
     def __len__(self):
         """Denotes the number of batches per epoch"""
-        return int(np.floor(len(self.dataset) / self.batch_size))
+        return len(self.dataset) // self.batch_size
 
     def __getitem__(self, index):
         """Generate one batch of data"""
