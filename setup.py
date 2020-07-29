a4d5fb8648ae6037f39a18c5bcdb00186d47d8e7
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-07-29 12:46:24 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mylib_maureen',
-    version='2.2.3',
+    version='2.2.4',
     packages=find_packages(),
     url='https://github.com/mohsu/mylib',
     author='Maureen Hsu',
