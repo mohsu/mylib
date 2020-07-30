f84cb578d48826f5aad3d0ae0f9027cf838ef399
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-07-30 12:46:39 +0800

    convert weight to float

--- cv/yolov3/core/layers.py
+++ cv/yolov3/core/layers.py
@@ -423,10 +423,10 @@ def YoloLoss(anchors, classes=[80], ignore_thresh=0.5, weights=[1, 1, 1, 1]):
             start_idx = end_idx
 
         # 6. sum over (batch, gridx, gridy, anchors) => (batch, 1)
-        xy_loss = tf.reduce_sum(xy_loss, axis=(1, 2, 3)) * weights[0]
-        wh_loss = tf.reduce_sum(wh_loss, axis=(1, 2, 3)) * weights[1]
-        obj_loss = tf.reduce_sum(obj_loss, axis=(1, 2, 3)) * weights[2]
-        class_loss = tf.reduce_sum(class_loss, axis=(1, 2, 3)) * weights[3]
+        xy_loss = tf.reduce_sum(xy_loss, axis=(1, 2, 3)) * float(weights[0])
+        wh_loss = tf.reduce_sum(wh_loss, axis=(1, 2, 3)) * float(weights[1])
+        obj_loss = tf.reduce_sum(obj_loss, axis=(1, 2, 3)) * float(weights[2])
+        class_loss = tf.reduce_sum(class_loss, axis=(1, 2, 3)) * float(weights[3])
 
         return xy_loss + wh_loss + obj_loss + class_loss
 
