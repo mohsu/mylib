aacbbe49042eae5a4b309ca8b3ec35a02818985a
Maureen Hsu <maureen.hsu@neutec.com.tw>
2020-08-03 10:09:09 +0800

    false posistive --> rate

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mylib_maureen',
-    version='2.2.8',
+    version='2.2.9',
     packages=find_packages(),
     url='https://github.com/mohsu/mylib',
     author='Maureen Hsu',
