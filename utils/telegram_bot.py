a4c03d28e2bd3a8fd7786d39cfd0a99643864951
maureen <maureen.hsu@neutec.com.tw>
2020-12-08 14:59:00 +0800

    save tg_bot group_id in class para

--- utils/telegram_bot.py
+++ utils/telegram_bot.py
@@ -14,15 +14,19 @@ from typing import Any, Union, Optional
 
 
 class TgBot:
-    def __init__(self, token: Any):
+    def __init__(self, token: Any, telegram_group_id: Optional[Union[str, int]] = None):
         self.token = token
         self.send_photo_url = "https://api.telegram.org/bot{}/sendPhoto".format(token)
+        self.telegram_group_id = telegram_group_id
 
-    def send_message(self, telegram_group_chat_id: Union[str, int],
-                     text: Any, parse_mode: Optional[Any] = None) -> requests.Response:
+    def send_message(self, telegram_group_chat_id: Optional[Union[str, int]] = None,
+                     text: Optional[Any] = None, parse_mode: Optional[Any] = None) -> requests.Response:
         """
         Send message from telegram bot in group
         """
+        if telegram_group_chat_id is None:
+            telegram_group_chat_id = self.telegram_group_id
+
         request = "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(self.token,
                                                                                          telegram_group_chat_id,
                                                                                          text)
@@ -32,7 +36,8 @@ class TgBot:
 
         return r
 
-    def send_photo(self, image_io, telegram_group_chat_id, caption=None) -> requests.Response:
+    def send_photo(self, image_io, telegram_group_chat_id: Optional[Union[str, int]] = None,
+                   caption: Optional[str] = None) -> requests.Response:
         """
         Send photo from telegram bot in group
         :param image_io: files = {'photo': open(image_path), 'rb')}
@@ -40,6 +45,9 @@ class TgBot:
         :param caption
         :return: response
         """
+        if telegram_group_chat_id is None:
+            telegram_group_chat_id = self.telegram_group_id
+
         data = {'chat_id': telegram_group_chat_id}
         if caption:
             data["caption"] = caption
